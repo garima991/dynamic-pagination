3156a640851bcc884a33144fa5033aab4d71d6d6
Garima <saxenagarima2953@gmail.com>
2024-11-04 03:41:14 +0530

    improved the logic

--- script1.js
+++ script1.js
@@ -7,7 +7,7 @@ const next = document.getElementById("next");
 
 let totalPages = 0;
 let curr = 1;
-const pageSize = 8;
+const pageSize = 4;
 let totalItems = 0;
 let recipes = [];
 
@@ -17,17 +17,18 @@ function fetchData(){
     .then((data) => {
         console.log(data);
         recipes = data.recipes;
-        totalItems = data.total;
+        totalItems = data.recipes.length;
         totalPages = Math.ceil(totalItems / pageSize);
         modify();
         addPagination();
     });
 }
 
-function modify(data){
+function modify(){
     const newRecipeData = recipes.slice((curr-1)*pageSize, curr*pageSize);
     showCards(newRecipeData);
     setActive();
+    performTruncation();
 }
 
 function showCards(recipes) {
@@ -46,7 +47,8 @@ function showCards(recipes) {
 let pages = [];
 
 function addPagination(){
-    for(let i = 1; i <= totalPages; i++){
+    pagination.innerHTML = "";
+    for(let i = 0; i < totalPages; i++){
         const page = document.createElement("button");
         page.textContent = i;
         page.classList.add("btn");
@@ -55,22 +57,23 @@ function addPagination(){
     }
 
     addEventListenersToPages();
-    // setActive();
+    setActive();
+    performTruncation();
 }
 
 function addEventListenersToPages(){
     for(let page of pages){
         page.addEventListener("click", () => {
-            curr = page.innerText;
+            curr = Number(page.innerText);
             modify();
         });
     }
 
-    prev.addEventListener("click", (event) => {
+    prev.addEventListener("click", () => {
         if(curr > 1) curr--;
         modify();
     });
-    next.addEventListener("click", (event) => {
+    next.addEventListener("click", () => {
         if(curr < totalPages) curr++;
         modify();
     });
@@ -78,12 +81,52 @@ function addEventListenersToPages(){
 }
 
 function setActive(){
+    prev.style.backgroundColor = curr === 1 ? "rgb(48, 111, 212)" : "white";
+    next.style.backgroundColor = curr === totalPages ? "rgb(48, 111, 212)" : "white";
+    prev.style.pointerEvents = curr === 1 ? "none" : "all";
+    next.style.pointerEvents = curr === totalPages ? "none" : "all";
+
+    // if(curr === 1){
+    //     prev.style.visibility = "hidden";
+    // }
+    // else{
+    //     prev.style.visibility = "visible";
+    // }
+
+    // if(curr === totalPages){
+    //     next.style.visibility = "hidden";
+    // }
+    // else{
+    //     next.style.visibility = "visible";
+    // }
     pages.forEach((page) => {
-        if(curr === Number(page.innerText))
-            page.classList.add("active");
-        else
-            page.classList.remove("active");
+        page.classList.remove("active")
     });
+    pages[curr-1]?.classList?.add("active");
+}
+
+
+function performTruncation(){
+    pages.forEach((page) => {
+        let num = Number(page.innerText);
+        let truncate = (num > 2) && (num < curr - 1 || num > curr + 1) && (num < totalPages - 1); 
+        let notTruncate = (num <= 5 && curr < 5) || (num >= totalPages-4 && curr > totalPages-4); 
+        let changetoDots = (num === 2 && curr > 3) || (num === totalPages - 1 && curr < totalPages - 3); 
+        
+        if(truncate && !notTruncate){
+            page.style.display = "none";
+        }
+        else{
+            page.style.display = "block";
+        }
+        if (changetoDots){
+            page.classList.add("truncated");
+        }
+        else{
+            page.classList.remove("truncated");
+        }
+      });
+    
 }
 
 fetchData();
